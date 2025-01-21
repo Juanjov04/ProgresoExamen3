f32b8a43f81cca3162d7b86c357ebd79e23a57cf
Juanjov04 <juan.villalba.ruiz@udla.edu.ec>
2025-01-20 19:52:29 -0500

    nuevos cambios

--- Buscar.xaml.cs
+++ Buscar.xaml.cs
@@ -1,9 +1,48 @@
+using SQLite;
+using static ProgresoExamen3.Paises;
+
 namespace ProgresoExamen3;
 
 public partial class Buscar : ContentPage
 {
-	public Buscar()
-	{
-		InitializeComponent();
-	}
+    private SQLiteAsyncConnection basededatos;
+
+    public Buscar()
+    {
+        InitializeComponent();
+        iniciobase();
+    }
+
+    private async void iniciobase()
+    {
+      
+        string rutaBD = Path.Combine(FileSystem.AppDataDirectory, "inicio.db3");
+        basededatos = new SQLiteAsyncConnection(rutaBD);
+        await basededatos.CreateTableAsync<Pais>();
+
+        
+        await CargarPaises();
+    }
+
+    private async Task CargarPaises()
+    {
+        
+        var paises = await basededatos.Table<Pais>().ToListAsync();
+
+        
+        if (paises.Count == 0)
+        {
+            await DisplayAlert("Información", "No se encontraron países en la base de datos.", "OK");
+            return;
+        }
+
+        
+        var paisesFormateados = paises.Select(p => new
+        {
+            DisplayText = $"Nombre País: {p.NombreOficial}, Región: {p.Region}, Link: {p.Google}, NombreBD: Scordova"
+        }).ToList();
+
+        
+        listViewPaises.ItemsSource = paisesFormateados;
+    }
 }
